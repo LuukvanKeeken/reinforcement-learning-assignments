c30eed9554632660a467efd2ec50ae7b2df98d5a
Luuk van Keeken <luukvankeeken24@gmail.com>
2021-01-17 11:43:47 +0100

    Improved the plotting file.

--- Assignment2/plotting.py
+++ Assignment2/plotting.py
@@ -3,14 +3,24 @@
 
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 def main(args):
-    dfTestRun = pd.read_csv("output-files/q-learning/epsilon-greedy/runs-100_games-200_alpha-0.300000_gamma-0.900000_epsilon-0.100000.csv")
+    dfTestRun = pd.read_csv("output-files/q-learning/upper-confidence-bound/runs-200_games-230_alpha-0.300000_gamma-1.000000_c-0.008000.csv")
+    trend1 = np.polyfit(dfTestRun['game number'], dfTestRun['percentage won'], 3)
+    trend2 = np.polyfit(dfTestRun['game number'], dfTestRun['percentage lost'], 3)
+    trend3 = np.polyfit(dfTestRun['game number'], dfTestRun['percentage draw'], 3)
+    p = np.poly1d(trend1)
+    p2 = np.poly1d(trend2)
+    p3 = np.poly1d(trend3)
 
     testFig = plt.figure(1)
     plt.plot(dfTestRun['game number'], dfTestRun['percentage won'], label = 'won')
+    plt.plot(dfTestRun['game number'], p(dfTestRun['game number']), color = 'blue')
     plt.plot(dfTestRun['game number'], dfTestRun['percentage lost'], label = 'lost')
+    plt.plot(dfTestRun['game number'], p2(dfTestRun['game number']), color = 'orange')
     plt.plot(dfTestRun['game number'], dfTestRun['percentage draw'], label = 'draw')
+    plt.plot(dfTestRun['game number'], p3(dfTestRun['game number']), color = 'green')
     plt.ylabel('% won/lost/drawn')
     plt.xlabel('game number')
     plt.title("Win/lose/draw percentages for each game in sequence")
